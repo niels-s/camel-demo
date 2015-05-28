1229c854488dac9666a9b4d27f6508f80edad506
Niels Stevens <niels@kabisa.nl>
2015-05-28 21:16:59 +0200

    Initial Commit

---
+++ src/main/java/com/demo/HealthCheckApp.java
@@ -0,0 +1,24 @@
+package com.demo;
+
+import com.demo.data.EmbeddedElasticServer;
+import com.demo.route.AlertsRouteBuilder;
+import com.demo.route.HealthCheckRouteBuilder;
+import com.demo.route.StatsRouteBuilder;
+import org.apache.camel.main.Main;
+
+public class HealthCheckApp {
+
+    public static void main(String... args) throws Exception {
+        EmbeddedElasticServer elasticServer = new EmbeddedElasticServer();
+
+        Main main = new Main();
+        main.enableHangupSupport();
+        main.addRouteBuilder(new HealthCheckRouteBuilder());
+        main.addRouteBuilder(new AlertsRouteBuilder());
+        main.addRouteBuilder(new StatsRouteBuilder(elasticServer.getClient()));
+        main.run(args);
+
+        elasticServer.shutdown();
+    }
+}
+
